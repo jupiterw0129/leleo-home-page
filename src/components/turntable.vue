77b1facb48169f52cd2986952098f2dd3eeebae4
World <jupiterw0129@qq.com>
2026-03-20 15:08:19 +0800

    Update turntable.vue

--- src/components/turntable.vue
+++ src/components/turntable.vue
@@ -8,7 +8,9 @@
   display: flex;
   justify-content: center;
   align-items: center;
+  filter: drop-shadow(0 0 8px v-bind(color2)) drop-shadow(0 0 18px v-bind(color2));
 }
+
 .svg-frame svg {
   position: absolute;
   transition: .5s;
@@ -17,19 +19,23 @@
   width: 344px;
   height: 344px;
   fill: none;
+  filter: drop-shadow(0 0 6px v-bind(color2)) drop-shadow(0 0 12px v-bind(color2));
 }
 
 .svg-frame:hover svg {
   transform: rotate(-80deg) skew(30deg) translateX(calc(45px * var(--i))) translateY(calc(-35px * var(--i)));
+  filter: drop-shadow(0 0 10px v-bind(color2)) drop-shadow(0 0 24px v-bind(color2)) drop-shadow(0 0 36px v-bind(color1));
 }
 
 .svg-frame svg #center {
   transition: .5s;
   transform-origin: center;
+  filter: drop-shadow(0 0 6px v-bind(color1)) drop-shadow(0 0 14px v-bind(color1));
 }
 
 .svg-frame:hover svg #center {
   transform: rotate(-30deg) translateX(45px) translateY(-3px);
+  filter: drop-shadow(0 0 10px v-bind(color1)) drop-shadow(0 0 20px v-bind(color1)) drop-shadow(0 0 30px v-bind(color2));
 }
 
 #out2 {
